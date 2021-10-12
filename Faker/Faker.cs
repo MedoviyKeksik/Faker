07873b964ab39d589b006343f46f09f94bebd547
Klim <Severin.Klim@yandex.by>
2021-10-13 00:09:21 +0300

    Implemented faker interface

---
+++ Faker/Faker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Faker
+{
+    public class Faker
+    {
+        FakerConfig _fakerConfig;
+
+        static Faker()
+        {
+
+        }
+
+        Faker()
+        {
+            _fakerConfig = new FakerConfig();
+        }
+
+        Faker(FakerConfig fakerConfig) 
+        {
+            _fakerConfig = fakerConfig;
+        }
+
+        public T Create<T>() where T : class
+        {
+            return null;
+        }
+    }
+}
