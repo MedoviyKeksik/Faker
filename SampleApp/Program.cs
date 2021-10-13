6301b4e87b7d5fe0616d005666db175599ca1a36
Klim <Severin.Klim@yandex.by>
2021-10-13 18:05:05 +0300

    Updated sample app

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -7,25 +7,42 @@ namespace SampleApp
 {
     class Sample
     {
-        public Sample() { }
+        public Sample(DateTime date) {
+            this.date = date.AddYears(1);
+        }
         public List<int> list;
-        public DateTime date;
+        public double someDouble { get; set; }
+        public Uri uri;
+        internal DateTime date;
+        public DateTime GetDate()
+        {
+            return date;
+        }
     }
 
-    [Generate(typeof(string))]
-    class CustomIntGenerator : IGenerator
+    [Generate(typeof(DateTime))]
+    class CustomDatetimeGenerator : IGenerator
     {
         public object Generate(IFakerContext randomProvider)
         {
-            return "asd";
+            return DateTime.Parse("2021-10-13 18:00");
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Faker.Faker faker = new Faker.Faker();
-            Console.WriteLine(faker.Create<Sample>().date);
+            var config = new FakerConfig();
+            config.Add<Sample, DateTime, CustomDatetimeGenerator>(obj => obj.date);
+            Faker.Faker faker = new Faker.Faker(config);
+            var tmp = faker.Create<Sample>();
+            Console.WriteLine(tmp.GetDate());
+            Console.WriteLine(tmp.list.Count);
+            for (int i = 0; i < tmp.list.Count; i++) {
+                Console.WriteLine(tmp.list[i]);
+            }
+            Console.WriteLine("Double: " + tmp.someDouble);
+            Console.WriteLine("URI: " + tmp.uri);
         }
     }
 }
